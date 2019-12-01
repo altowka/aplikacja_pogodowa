27c18118f011c436fa3bc77e6c43ec1cf33ea496
altowka <markiewiczmaria23@gmail.com>
2019-12-01 22:35:11 +0100

    added animated photos(not on small screen)

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function () {
     const towns = document.getElementById("towns");
-    const weatherDiv = document.getElementById("weather")
+    const weatherDiv = document.getElementById("weather");
+    let image = document.getElementById("animated-img");
     let cityName;
 
     function getPosts(e) {
@@ -19,23 +20,51 @@ window.addEventListener('load', function () {
                 let wind = data.wind
                 for (let key in weatherData) {
                     output += `<li class="list-group-item">${key}: ${Math.round(Math.floor(weatherData[key]))}${addUnits(key)}<div  id="${key}" class="img"></div></li>`
+                    displayImages(weatherData, key);
                 }
                 for (let key in wind) {
                     output += `<li class="list-group-item">wind-${key}: ${Math.round(Math.floor(wind[key]))}${addUnits(key)}<div  id="${key}" class="img"></div></li>`
                 }
 
                 weatherDiv.innerHTML = `<ul class="list-group mb-3">${output}</ul>`;
-                console.log(data)
+
             })
             .catch((err) => {
                 console.log(err)
                 weatherDiv.innerHTML = err;
+                image.style.display = "none"
             })
 
     }
 
+    function displayImages(object, key) {
+        image.style.display = "block";
+        image.style.animation = "fadeIn ease 5s"
+        image.style.backgroundRepeat = "no-repeat";
+        if (key === "temp") {
+            if (object[key] > 15) {
+                image.style.backgroundImage = "url('img/animations.jpg')"
+                image.style.backgroundPosition = "0% -5%"
+            } else if (object[key] > 2) {
+                image.style.backgroundImage = "url('img/animations.jpg')"
+                image.style.backgroundPosition = "0% 40%"
 
-    function addUnits(key) {
+            } else if (object[key] > -2) { //średnia minka
+                image.style.backgroundImage = "url('img/animations.jpg')"
+                image.style.backgroundPosition = "50% 40%"
+
+            } else {
+                image.style.backgroundImage = "url('img/animations.jpg')"
+                image.style.backgroundPosition = "53% 85%"
+
+            }
+
+        }
+    }
+
+
+
+    function addUnits(object, key) {
         switch (key) {
             case "temp":
             case "temp_min":
@@ -52,11 +81,11 @@ window.addEventListener('load', function () {
                 return "m/s";
                 break;
             case "deg":
-                if (key >= 0 && key < 90) {
+                if (object[key] >= 0 && object[key] < 90) {
                     return " (north)"
-                } else if (key >= 90 && key < 180) {
+                } else if (object[key] >= 90 && object[key] < 180) {
                     return " (east)"
-                } else if (key >= 180 && key < 270) {
+                } else if (object[key] >= 180 && object[key] < 270) {
                     return " (south)"
                 } else {
                     return " (west)"
@@ -68,4 +97,7 @@ window.addEventListener('load', function () {
     }
 
     towns.addEventListener('change', getPosts)
+    towns.addEventListener('change', () => {
+        image.style.removeProperty("animation")
+    })
 })
